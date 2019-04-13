f517401767c6be37b39279511819888835fc058f
HuoHuoMurphy <2504117160@qq.com>
2019-04-13 22:38:36 +0800

    祸祸 4/13
    关于UI方面、消息群发和场景切换的更新

---
+++ Assets/Test/UITest/UITest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Common;
+using DMZL_UI;
+using UnityEngine;
+
+public class UITest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIManager.I.GetWindow<UI_SystemButton>().Show();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
